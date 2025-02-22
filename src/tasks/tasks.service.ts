2ed2ef4c80965c1b2812df0253b1419bcc8d0229
Andrei Vlasov <vlasov124356@gmail.com>
2025-02-22 16:23:10 +0300

    feat: Add TaskService with task execution and task taking methods

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DataSource } from 'typeorm';
+import { Task, TaskStatus } from './task.entity';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
+@Injectable()
+export class TaskService {
+  constructor(
+    @InjectRepository(Task)
+    private readonly taskRepository: Repository<Task>,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async taskExecution(): Promise<void> {
+    try {
+    } catch (error) {
+      console.log('Failed to handle tasks');
+    }
+  }
+
+  async takeTask(): Promise<Task | null> {
+    return await this.dataSource.transaction(async (manager) => {
+      // Выбираем задачу со статусом 'pending' с блокировкой строки (pessimistic write)
+      const task = await manager
+        .getRepository(Task)
+        .createQueryBuilder('task')
+        .setLock('pessimistic_write')
+        .where('task.status = :status', { status: TaskStatus.NEW })
+        .orderBy('task.createdAt', 'ASC')
+        .getOne();
+
+      if (!task) {
+        return null;
+      }
+
+      task.status = TaskStatus.PROCESSING;
+      await manager.getRepository(Task).save(task);
+
+      return task;
+    });
+  }
+}
